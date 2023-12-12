fe8fa885db260a5713f3eaadc1d670966bccd350
ArtemMakovskyy <photomak79@gmail.com>
2023-12-12 12:12:41 +0200

    first commit

---
+++ src/main/java/com/example/demomysqldocker/DemoMysqlDockerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demomysqldocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMysqlDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMysqlDockerApplication.class, args);
+        System.out.println("start + 12-12-2023");
+    }
+
+}
